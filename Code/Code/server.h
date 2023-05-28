62d5e257558876f03f1cf6adf0da0ff389ae583f
SooyaDolphin <120730055+SooyaDolphin@users.noreply.github.com>
2023-05-28 17:20:04 +0900

    주석 변경

--- Code/Code/server.h
+++ Code/Code/server.h
@@ -10,25 +10,25 @@
 #include "card.h"
 
 
-extern int turn; //누구 차례인지
+extern int turn; //누구 차례인지 확인
 extern int tt; //차례temp
-extern char *name[4]; //플레이어 이름
-extern struct card clntCards[4][56]; //플레이어 카드
-extern int num; //접속인원
-extern int clntCardNum[4]; //플레이어 카드 개수
+extern char *name[4]; //이름
+extern struct card clntCards[4][56]; //카드
+extern int num; //접속인원 확인
+extern int clntCardNum[4]; //카드 개수
 extern int tableCardNum[4]; //테이블 카드 개수
-extern pthread_t thread[4]; //플레이어 4명 쓰레드
-extern int status_num; //몇번 플레이어 상태가 변했는지
+extern pthread_t thread[4]; //4명 쓰레드
+extern int status_num; //누구 상태가 변했는지
 extern char status; //현재 상태 r,y,g,p:빨강,노랑,초록,보라색 카드 뒤집힘 /  o:벨 맞음 / x:벨 잘못누름 / e:게임종료
 extern int statusCheck[4]; //1 : 클라이언트로 보내야할거 있음, 0 : 다 보냄
 
-int bell(); //벨 누를 상황인지 체크
-void bell_O(int); //벨 누를 상황일때 테이블 카드 옮겨주기
-void bell_X(int); //벨 누를 상황 아닐때 테이블 카드 옮겨주기
+int bell(); //벨 누를 상황인지 아닌지 체크
+void bell_O(int); //벨 누를 상황일 경우 테이블 카드 옮겨주기
+void bell_X(int); //벨 누를 상황 아닐 경우 테이블 카드 옮겨주기
 void pullCard(int,int); //카드 앞으로 당겨줌
-void checkGameEnd(); //게임끝 체크
+void checkGameEnd(); //게임끝났을 때 체크
 
-int bell() //벨을 누를 상황인지 체크
+int bell() //벨을 누를 상황인지 확인
 {
 	struct card C[4];
 	C[0].color="red";
@@ -57,17 +57,17 @@ int bell() //벨을 누를 상황인지 체크
 
 	for(int i=0;i<4;i++)
 	{
-		if(C[i].num==5) //5개인 과일이 있다
+		if(C[i].num==5) //5개인 과일이 있는 경우
 			return 1;
 	}
-	return 0; //5개인 과일이 없다
+	return 0; //5개인 과일이 없는 경우
 }
 
 void bell_O(int threadNum)
 {
 	int sum=0,k=0,i,j;
 	for(i=0;i<4;i++)
-		sum+=tableCardNum[i]; //옮길 카드 개수 세기
+		sum+=tableCardNum[i]; //옮길 카드 개수 세는 부분 
 	struct card temp[sum];
 	for(i=0;i<4;i++)
 	{
@@ -85,10 +85,10 @@ void bell_O(int threadNum)
 	for(i=0;i<4;i++)
 	{
 		if(i==threadNum)
-			clntCardNum[threadNum]+=sum; //벨 누른 사람 카드 개수 증가
+			clntCardNum[threadNum]+=sum; //벨 누른 사람의 경우 카드 개수 증가
 		//else
-		pullCard(i,tableCardNum[i]); //테이블에 있던 카드 수 만큼 카드 구조체 당겨줌
-		tableCardNum[i]=0; //테이블 카드 없어짐
+		pullCard(i,tableCardNum[i]); //테이블에 있던 카드 수 만큼 카드 구조체 끌어옴
+		tableCardNum[i]=0; //테이블의 카드 없어짐
 	}
 }
 
@@ -98,23 +98,23 @@ void bell_X(int threadNum)
 	struct card temp[3];
 	for(i=(clntCardNum[threadNum]+tableCardNum[threadNum]-1);i>=clntCardNum[threadNum]+tableCardNum[threadNum]-3;i--)
 	{
-		temp[j]=clntCards[threadNum][i]; //옮길 카드 3장 temp에 임시저장
-		clntCards[threadNum][i].num=0; //옮긴 카드 없애기
+		temp[j]=clntCards[threadNum][i]; //옮길 카드 3장 temp에 임시로 저장
+		clntCards[threadNum][i].num=0; //옮긴 카드 없애주기
 		clntCards[threadNum][i].color=NULL;
 		j++;
 	}
 	j=0;
 	for(i=0;i<4;i++)
 	{
-		if(threadNum!=i) //벨 잘못 누르지 않은 플레이어
+		if(threadNum!=i) //벨 잘못 누르지 않은 플레이어의 경우
 		{
-			clntCards[i][clntCardNum[i]+tableCardNum[i]]=temp[j]; //카드 하나 받음
+			clntCards[i][clntCardNum[i]+tableCardNum[i]]=temp[j]; //카드 하나 획득
 			j++;
-			clntCardNum[i]++; //카드 한개 획득
+			clntCardNum[i]++; //카드 1개 획득
 		}
-		else //벨 잘못 누른 플레이어
+		else //벨 잘못 누른 플레이어의 경우
 		{
-			clntCardNum[i]-=3; //카드 3개 손실
+			clntCardNum[i]-=3; //카드 3개 손실 발생
 		}
 	}
 }
@@ -136,7 +136,7 @@ void pullCard(int threadNum, int howmany)
 void checkGameEnd()
 {
 	for(int i=0;i<4;i++)
-		if(clntCardNum[i]<=0) //플레이어 카드 0이하인지 확인
+		if(clntCardNum[i]<=0) //플레이어 카드 0이하인지 체크
 		{
 			status='e';
 			for(int j=0;j<4;j++)
